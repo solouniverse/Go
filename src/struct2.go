6e0d3155f0f93aa34b403970247ac166731e5d9a
srikantharya <srikantharya@LnuxWS-0.mshome.net>
2020-05-04 07:38:32 -0700

    Go

---
+++ src/struct2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+var student struct {
+	name  string
+	age   int
+	class int
+	perc  float64
+}
+
+func main() {
+	var s1 student
+	s1.name = "Om"
+	s1.age = 19
+	s1.class = 10
+	s1.perc = 35.5
+
+	fmt.Printf("%v\n", s1)
+	fmt.Printf("Datatype of student: %T\n", s1)
+}
